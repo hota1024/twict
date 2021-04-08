6bc80d6d26a02f9cdb32db3bec2a22108593c602
hota1024 <hotalog@hota1024.com>
2021-04-09 05:46:42 +0900

    🏷️ rename auth items

---
+++ src/types/UserCredentials.ts
@@ -0,0 +1,14 @@
+/**
+ * UserCredentials type.
+ */
+export type UserCredentials = {
+  /**
+   * access token.
+   */
+  token: string
+
+  /**
+   * access token secret.
+   */
+  secret: string
+}
